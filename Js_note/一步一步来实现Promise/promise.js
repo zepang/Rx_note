e9cdd07064f32dc421a5df9b2d4073dc53f9712c
zepang <ummlq4@gmail.com>
2018-11-22 19:40:55 +0800

    收藏一片关于Promise的实现的知乎文章

---
+++ Js_note/一步一步来实现Promise/promise.js
@@ -0,0 +1,35 @@
+function MyPromise(executor) {
+  var self = this
+  self.status = 'pending' // Promise当前的状态
+  self.data = undefined  // Promise的值
+  self.onResolvedCallback = [] // Promise resolve时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+  self.onRejectedCallback = [] // Promise reject时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+
+  function resolve(value) {
+    if (self.status === 'pending') {
+      self.status = 'resolved'
+      self.data = value
+      for(var i = 0; i < self.onResolvedCallback.length; i++) {
+        self.onResolvedCallback[i](value)
+      }
+    }
+  }
+
+  function reject(reason) {
+    if (self.status === 'pending') {
+      self.status = 'rejected'
+      self.data = reason
+      for(var i = 0; i < self.onRejectedCallback.length; i++) {
+        self.onRejectedCallback[i](reason)
+      }
+    }
+  }
+
+  try { // 考虑到执行executor的过程中有可能出错，所以我们用try/catch块给包起来，并且在出错后以catch到的值reject掉这个Promise
+    executor(resolve, reject) // 执行executor
+  } catch(e) {
+    reject(e)
+  }
+
+  // ...
+}
