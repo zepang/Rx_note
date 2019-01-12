841901a32203a3637b719bc51b401dd17f723495
zepang <ummlq4@gmail.com>
2019-01-12 09:33:08 +0800

    整理目录文件

---
+++ 书籍摘录/PractialNode.js2ndEdition/code/hello-debugger.js
@@ -0,0 +1,13 @@
+console.log(process.cwd())
+console.log(__dirname)
+
+const http = require('http')
+const port = 3000
+debugger
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  debugger  
+  res.end('Hello World\n')
+}).listen(3000, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
