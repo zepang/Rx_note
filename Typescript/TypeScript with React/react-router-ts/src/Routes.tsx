5650385858fe31c2b4eafb19104601367f6c06d0
zepang <ummlq4@gmail.com>
2019-03-01 18:38:17 +0800

    完成typescript和react-router的demo

--- Typescript/TypeScript with React/react-router-ts/src/Routes.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/Routes.tsx
@@ -1,18 +1,28 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Header from './Header'
 import AdminPage from './AdminPage'
 import ProductsPage from './ProductsPage'
 import ProductPage from './ProductPage'
+import LoginPage from './LoginPage'
+import NotFoundPage from './NotFoundPage'
 
 const Routes: React.SFC = () => {
+  const [loggedIn, setLoggedIn] = React.useState(true)
   return (
     <Router>
       <div>
         <Header />
-        <Route exact={true} path="/products" component={ProductsPage}></Route>
-        <Route path="/products/:id" component={ProductPage}></Route>
-        <Route path="/admin" component={AdminPage}></Route>
+        <Switch>
+          <Redirect exact={true} from="/" to="/products"></Redirect>
+          <Route exact={true} path="/products" component={ProductsPage}></Route>
+          <Route path="/products/:id" component={ProductPage}></Route>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/admin">
+            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+          </Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
       </div>
     </Router>
   )
