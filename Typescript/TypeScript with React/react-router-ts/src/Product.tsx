eced2e0e39bf8efc729732082d332f318ffbc128
zepang <ummlq4@gmail.com>
2019-03-06 16:16:53 +0800

    render props pattern

--- Typescript/TypeScript with React/react-router-ts/src/Product.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/Product.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { IProduct } from './ProductData'
+import Tabs from './Tabs'
 
 interface IProps {
   product: IProduct;
@@ -15,16 +16,20 @@ const Product: React.SFC<IProps> = (props) => {
   return (
     <React.Fragment>
       <h1>{product.name}</h1>
-        <p>{product.description}</p>
-        <div>
-          <ul className="product-reviews">
-            {product.reviews.map(review => (
-              <li key={review.reviewer} className="product-reviews-item">
-                <i>"{review.comment}"</i> - {review.reviewer}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Tabs>
+          <Tabs.Tab name='Description' initialActive={true} heading={() => <b>Description</b>}>
+            <p>{product.description}</p>
+          </Tabs.Tab>
+          <Tabs.Tab name="Reviews" heading={() => 'Reviews'}>
+            <ul className="product-reviews">
+              {product.reviews.map(review => (
+                <li key={review.reviewer} className="product-reviews-item">
+                  <i>"{review.comment}"</i> - {review.reviewer}
+                </li>
+              ))}
+            </ul>
+          </Tabs.Tab>
+        </Tabs>
         <p className="product-price">
           {
             new Intl.NumberFormat("en-US", {
