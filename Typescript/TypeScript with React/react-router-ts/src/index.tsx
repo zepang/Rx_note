0e374bbb2ef222ad09517406b7172b2979322752
zepang <ummlq4@gmail.com>
2019-03-11 18:18:45 +0800

    fix

--- Typescript/TypeScript with React/react-router-ts/src/index.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/index.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes'
+import { Provider } from 'react-redux'
+import { Store } from 'redux'
+import configureStore from './store'
+import { IApplicationState } from './store'
 
-ReactDOM.render(<Routes />, document.getElementById('root') as HTMLElement);
+interface IProps {
+  store: Store<IApplicationState>;
+}
+
+const Root: React.SFC<IProps> = (props) => {
+  return (
+    <Provider store={props.store}>
+      <Routes></Routes>
+    </Provider>
+  )
+} 
+
+ReactDOM.render(<Root store={configureStore()} />, document.getElementById('root') as HTMLElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
