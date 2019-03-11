648eb90bb585fdb28ff23d6ec9172b785dad2b31
zepang <ummlq4@gmail.com>
2019-03-11 18:14:19 +0800

    创建 actions reducers store

---
+++ Typescript/TypeScript with React/react-router-ts/src/ProductsActions.tsx
@@ -0,0 +1,22 @@
+import { ActionCreator, AnyAction, Dispatch } from "redux"
+import { ThunkAction } from 'redux-thunk'
+import { getProducts as getProductsFromAPI } from "./ProductsData"
+import { IProductsGetAllAction, IProductsLoadingAction, IProductsState, ProductsActionTypes } from "./ProductsTypes"
+
+const loading: ActionCreator<IProductsLoadingAction> = () => {
+  return {
+    type: ProductsActionTypes.LOADING
+  }
+}
+
+export const getProducts:
+ActionCreator<ThunkAction<Promise<AnyAction>, IProductsState, null, IProductsGetAllAction>> = () => {
+  return async (dispatch: Dispatch) => {
+    dispatch(loading())
+    const products = await getProductsFromAPI()
+    return {
+      products,
+      type: ProductsActionTypes.GETALL
+    }
+  }
+}
---
+++ Typescript/TypeScript with React/react-router-ts/src/ProductsTypes.ts
@@ -0,0 +1,23 @@
+import { IProduct } from "./ProductsData"
+
+export enum ProductsActionTypes {
+  GETALL = 'PRODUCTS/GETALL',
+  LOADING = 'PRODUCTS/LOADING'
+}
+
+export interface IProductsGetAllAction {
+  type: ProductsActionTypes.GETALL;
+  products: IProduct[];
+}
+
+export interface IProductsLoadingAction {
+  type: ProductsActionTypes.LOADING
+}
+
+
+export type ProductsActions = IProductsGetAllAction | IProductsLoadingAction
+
+export interface IProductsState {
+  readonly products: IProduct[];
+  readonly productsLoading: boolean;
+}
