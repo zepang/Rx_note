20531a5dca7704886a44ee47427add169c65e1a8
zepang <ummlq4@gmail.com>
2019-03-14 16:39:06 +0800

    Creating and connecting BasketSummary to the store

--- Typescript/TypeScript with React/react-router-ts/src/Header.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/Header.tsx
@@ -1,23 +1,30 @@
-import * as React from 'react'
+import * as React from "react"
 // NavLink 自动基于当前路由添加 class active
-import { NavLink, RouteComponentProps, withRouter } from 'react-router-dom'
+import { NavLink, RouteComponentProps, withRouter } from "react-router-dom"
 // import { Link } from 'react-router-dom'
-import logo from './logo.svg'
-import 'url-search-params-polyfill'
+import logo from "./logo.svg"
+import BasketSummary from "./BasketSummary"
+import { connect } from "react-redux"
+import { IApplicationState } from "./Store"
+import "url-search-params-polyfill"
 
-const Header: React.SFC<RouteComponentProps> = (props) => {
-  const [search, setSearch] = React.useState('')
+interface IProps extends RouteComponentProps {
+  basketCount: number
+}
+
+const Header: React.SFC<IProps> = props => {
+  const [search, setSearch] = React.useState("")
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.currentTarget.value)
   }
   const handleSearchKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       props.history.push(`/products?search=${search}`)
     }
   }
-  React.useEffect(() =>{
+  React.useEffect(() => {
     const searchParams = new URLSearchParams(props.location.search)
-    setSearch(searchParams.get('search') || '')
+    setSearch(searchParams.get("search") || "")
   }, [])
   return (
     <header className="header">
@@ -29,16 +36,29 @@ const Header: React.SFC<RouteComponentProps> = (props) => {
           onChange={handleSearchChange}
           onKeyDown={handleSearchKeydown}
         />
+        <BasketSummary count={props.basketCount} />
       </div>
-      <img src={logo} alt="logo" className="header-logo"/>
+      <img src={logo} alt="logo" className="header-logo" />
       <h1 className="header-title">React Shop</h1>
       <nav>
-        <NavLink className="header-link" to="/products">Products</NavLink>
-        <NavLink className="header-link" to="/admin">Admin</NavLink>
-        <NavLink className="header-link" to="/contactus">Contactus</NavLink>
+        <NavLink className="header-link" to="/products">
+          Products
+        </NavLink>
+        <NavLink className="header-link" to="/admin">
+          Admin
+        </NavLink>
+        <NavLink className="header-link" to="/contactus">
+          Contactus
+        </NavLink>
       </nav>
     </header>
   )
 }
 
-export default withRouter(Header)
+const mapStateToProps = (store: IApplicationState) => {
+  return {
+    basketCount: store.basket.products.length
+  }
+}
+
+export default connect(mapStateToProps)(withRouter(Header))
