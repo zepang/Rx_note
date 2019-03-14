fb5414d5e0002dfb10dd3d72563f6fab38cb3ad9
zepang <ummlq4@gmail.com>
2019-03-14 16:30:57 +0800

    Connecting ProductPage to the store

--- Typescript/TypeScript with React/react-router-ts/src/ProductPage.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/ProductPage.tsx
@@ -1,64 +1,80 @@
-import * as React from 'react'
-import { RouteComponentProps, Prompt } from 'react-router-dom'
-import { IProduct, products, getProduct } from './ProductData'
-import Product from './Product'
+import * as React from "react"
+import { RouteComponentProps, Prompt } from "react-router-dom"
+import { IProduct } from "./ProductData"
+import Product from "./Product"
+import { connect } from "react-redux"
+import { addToBasket } from "./BasketActions"
+import { getProduct } from "./ProductsActions"
+import { IApplicationState } from "./Store"
 
-type Props = RouteComponentProps<{id: string}>
-
-interface IState {
-  product?: IProduct;
-  added: boolean;
-  loading: boolean;
+interface IProps extends RouteComponentProps<{ id: string }> {
+  addToBasket: typeof addToBasket
+  getProduct: typeof getProduct
+  loading: boolean
+  product?: IProduct
+  added: boolean
 }
 
-class ProductPage extends React.Component<Props, IState> {
-  public constructor(props: Props) {
+class ProductPage extends React.Component<IProps, {}> {
+  public constructor(props: IProps) {
     super(props)
-    this.state = {
-      added: false,
-      loading: true
-    }
   }
 
-  public async componentDidMount () {
+  public componentDidMount() {
     if (this.props.match.params.id) {
       const id: number = parseInt(this.props.match.params.id, 10)
-      const product = await getProduct(id)
-      if (product !== null) {
-        this.setState({ product, loading: false })
-      }
+      this.props.getProduct(id)
     }
   }
 
-  private handleAddClick = () =>  {
-    this.setState({ added: true })
+  private handleAddClick = () => {
+    if (this.props.product) {
+      this.props.addToBasket(this.props.product)
+    }
   }
 
-  private navAwayMessage = () => "Are you sure you leave without buying this product?"
+  private navAwayMessage = () =>
+    "Are you sure you leave without buying this product?"
 
-  public render () {
-    const product = this.state.product
+  public render() {
+    const product = this.props.product
     return (
       <div className="page-container">
-        <Prompt when={!this.state.added}
-          message={this.navAwayMessage}
-        />
-      {
-        product || this.state.loading ? (
+        <Prompt when={!this.props.added} message={this.navAwayMessage} />
+        {product || this.props.loading ? (
           <Product
-            loading={this.state.loading}
+            loading={this.props.loading}
             product={product}
-            inBasket={this.state.added}
+            inBasket={this.props.added}
             onAddToBasket={this.handleAddClick}
           />
         ) : (
           <p>products not found!</p>
-        )
-      }
+        )}
       </div>
     )
   }
 }
 
-export default ProductPage
+const mapStateTopProps = (store: IApplicationState) => {
+  return {
+    added: store.basket.products.some(p =>
+      store.products.currentProduct ? p.id ===
+      store.products.currentProduct.id : false),
+    basketProducts: store.basket.products,
+    loading: store.products.productsLoading,
+    product: store.products.currentProduct || undefined
+  }
+}
 
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    addToBasket: (product: IProduct) => dispatch(addToBasket(product)),
+    getProduct: (id: number) => dispatch(getProduct(id))
+  }
+}
+
+export default connect(
+  mapStateTopProps,
+  mapDispatchToProps
+)(ProductPage)
--- Typescript/TypeScript with React/react-router-ts/src/store.tsx
+++ Typescript/TypeScript with React/react-router-ts/src/store.tsx
@@ -2,13 +2,17 @@ import { applyMiddleware, combineReducers, createStore, Store } from 'redux'
 import thunk from "redux-thunk";
 import { productsReducer } from "./ProductsReducer";
 import { IProductsState } from "./ProductsTypes";
+import { basketReducer } from './BasketReducer'
+import { IBasketState } from './BasketTypes'
 
 export interface IApplicationState {
-  products: IProductsState
+  products: IProductsState,
+  basket: IBasketState
 }
 
 const rootReducer = combineReducers<IApplicationState>({
-  products: productsReducer
+  products: productsReducer,
+  basket: basketReducer
 })
 
 export default function configureStore ():Store {
