54bad5cd054f8e46200949a652c73190456ab813
zepang <ummlq4@gmail.com>
2019-03-20 17:53:50 +0800

    使用apolloe 请求数据

--- Typescript/TypeScript with React/repo-search-ts/src/App.tsx
+++ Typescript/TypeScript with React/repo-search-ts/src/App.tsx
@@ -1,15 +1,28 @@
 import React, { Component } from "react"
 import "./App.css"
 import { Header } from "./Header"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
+
+const AuthorizationToken = "fa208ce234b57958288717a8a958a34e41336e4f"
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `Bearer ${AuthorizationToken}`
+  }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Header />
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+          </header>
+        </div>
+      </ApolloProvider>
     )
   }
 }
