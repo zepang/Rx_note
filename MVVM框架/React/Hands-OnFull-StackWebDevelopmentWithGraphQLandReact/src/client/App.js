acf056f3726f31a6e9850bed0155940471a8b5b7
laizehai <laizehai@tulong.com>
2019-03-27 15:12:10 +0800

    添加eslint和babel的配置，使得他们支持property initializers的写法，并记录到文档

--- MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/client/App.js
+++ MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/client/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 
+const posts = [
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: { avatar: '/uploads/avatar1.png', username: 'Test User 1' }
+  },
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: { avatar: '/uploads/avatar2.png', username: 'Test User 2' }
+  }
+]
+
 export default class App extends Component {
-  render () {
+  state = {
+    posts: post
+  }
+  render() {
+    const { posts } = this.state
     return (
-      <div>Hello World!</div>
+      <div className="container">
+        <div className="feed">
+          {posts.map((post, i) => (
+            <div className="post" key={i}>
+              <div className="header">
+                <img src={post.user.avtar} alt="" />
+                <h3>{post.user.name}</h3>
+              </div>
+              <p className="content">{post.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     )
   }
 }
