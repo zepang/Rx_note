8b1f8ef258dc004d80f392dc7863892d03adde29
zepang <ummlq4@gmail.com>
2019-03-27 18:00:33 +0800

    安装启动express server

---
+++ MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/webpack.client.build.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [path.resolve('node_modules')],
+        include: [path.resolve('src')],
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
