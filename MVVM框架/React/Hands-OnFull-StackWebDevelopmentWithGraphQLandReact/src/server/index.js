3f59a6da1194374a6089b848e44ec98d3f329218
zepang <ummlq4@gmail.com>
2019-03-28 15:03:22 +0800

    Hands-OnFull-StackWebDevelopmentWithGraphQLandReact 修改文档和server demo 代码

--- MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/index.js
+++ MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/index.js
@@ -1,9 +1,21 @@
+import path from 'path'
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import compress from 'compress'
+const root = path.join(__dirname, '../../')
 
 const app = express()
 
-app.get('*', (req, res) => {
-  res.send('hello world!')
+app.use(helmet())
+app.use(compress())
+app.use(cors())
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
+
+app.use('/', express.static(path.join(root, 'dist/client')))
+app.use('/uploads', express.static(path.join(root, 'uploads')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, 'dist/client/index.html'))
 })
 
 app.listen(8000, () => {
