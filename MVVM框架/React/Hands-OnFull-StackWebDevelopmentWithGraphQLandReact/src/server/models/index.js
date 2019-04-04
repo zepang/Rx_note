c9330fa21753c73286d925888daa8e63f33cac78
zepang <ummlq4@gmail.com>
2019-04-04 18:39:28 +0800

    暂存

--- MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/models/index.js
+++ MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/models/index.js
@@ -1,21 +1,14 @@
 import Sequelize from 'sequelize'
-// import requireContext from 'babel-plugin-macros/context'
-import logger from '../helpers/logger'
-
 if (process.env.NODE_ENV === 'development') {
-  debugger
-  logger.log({ level: 'info', message: 'development' })
   const register = require('babel-plugin-require-context-hook/register')
-  logger.log({ level: 'info', message: register })
   register()
 }
 
 export default (sequelize) => {
   let db = {}
 
-  const context = requireContext(__dirname, '', true, /^\.\/(?!index\.js).*\.js$/)
-  console.log(__dirname, context)
-  context.keys.forEach(module => {
+  const context = require.context('.', true, /^\.\/(?!index\.js).*\.js$/, 'sync')
+  context.keys().map(context).forEach(module => {
     const model = module(sequelize, Sequelize)
     db[model.name] = model
   })
