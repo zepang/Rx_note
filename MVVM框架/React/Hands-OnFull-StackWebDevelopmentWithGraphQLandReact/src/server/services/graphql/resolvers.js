ead406cd1f5868fb6424eefa7160a4b94113530c
zepang <ummlq4@gmail.com>
2019-05-06 18:57:08 +0800

    添加react graphql项目的一些代码

--- MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/services/graphql/resolvers.js
+++ MVVM框架/React/Hands-OnFull-StackWebDevelopmentWithGraphQLandReact/src/server/services/graphql/resolvers.js
@@ -1,40 +1,26 @@
-import logger from '../../helpers/logger'
-let posts = [
-  {
-    id: 2,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar1.png',
-      username: 'Test User'
-    }
-  },
-  {
-    id: 1,
-    text: 'Lorem ipsum',
-    user: {
-      avatar: '/uploads/avatar2.png',
-      username: 'Test User 2'
-    }
-  }
-]
-const resolvers = {
-  RootQuery: {
-    posts(root, args, context) {
-      return posts
-    }
-  },
-  RootMutation: {
-    addPost(root, { post, user }, context) {
-      const postObject = {
-        ...post,
-        user,
-        id: posts.length + 1
-      }
-      posts.push(postObject)
-      logger.log({ level: 'info', message: 'Post was created' })
-      return postObject
-    }
-  }
-}
+// import logger from '../../helpers/logger'
 
-export default resolvers
+export default function resolver () {
+  const { db } = this
+  const { Post } = db.models
+  const resolvers = {
+    RootQuery: {
+      posts(root, args, context) {
+        return Post.findAll({ order: [['createdAt', 'DESC']] })
+      }
+    },
+    RootMutation: {
+      // addPost(root, { post, user }, context) {
+      //   const postObject = {
+      //     ...post,
+      //     user,
+      //     id: posts.length + 1
+      //   }
+      //   posts.push(postObject)
+      //   logger.log({ level: 'info', message: 'Post was created' })
+      //   return postObject
+      // }
+    }
+  }
+  return resolvers
+}
