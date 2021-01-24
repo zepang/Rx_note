52036658835744d6aed3a769f1629b60cb132133
zepang <ummlq4@gmail.com>
2021-01-24 15:55:52 +0800

    删除一些收藏的网页文件

--- 打包、构建和编译相关工具/webpack/SPA_Webpack/advanced/src/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-// 引入 router
-import router from './router'
-new Vue()
-
-// 启动 router
-router.start()
--- 打包、构建和编译相关工具/webpack/SPA_Webpack/sample/src/index.js
+++
@@ -1,5 +0,0 @@
-// 引入 router
-import router from './router'
-
-// 启动 router
-router.start()
